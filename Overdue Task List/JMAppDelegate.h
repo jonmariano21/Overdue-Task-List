120a0d760e6c2f735f2cffb2e45148074c66d0f0
jonmariano21 <jonmariano21@gmail.com>
2014-06-22 13:08:53 -0700

    Initial Commit

---
+++ Overdue Task List/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  Overdue Task List
+//
+//  Created by JONATHAN MARIANO on 6/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Overdue Task List/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  Overdue Task List
+//
+//  Created by JONATHAN MARIANO on 6/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
